59beafc7596c01c208a272314cdf50f5ce49bbd2
Yaniv M <yaniv.michaelis@wework.com>
2019-08-09 15:53:26 +0300

    Added mute

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import cn from 'classnames';
+
 import Button from './Button';
 import sound1 from './sounds/simonSound1.mp3'
 import sound2 from './sounds/simonSound2.mp3'
@@ -9,12 +11,18 @@ class Board extends React.Component {
   state = {
     clicked: 0,
     listOfClicks: [],
+    mute: true,
     timer: 250, // ms, time to show clicked
   }
 
-  play = (type) => {
-    const playPromise = document.getElementById(`simon${type}`).play();
+  playSound = (type) => {
+    // const playPromise = document.getElementById(`simon${type}`).play();
     // playPromise.then().catch();
+
+    // OR sound = new Audio(mp3).play()
+    if (!this.state.mute) {
+      document.getElementById(`simon${type}`).play();
+    }
   }
 
   select = (type) => {
@@ -22,8 +30,7 @@ class Board extends React.Component {
       this.state.listOfClicks.push(type);
       console.log(this.state.listOfClicks);
 
-      // play tune
-      this.play(type);
+      this.playSound(type);
       this.setState(({clicked: type}), this.diselect );
       };
   }
@@ -31,8 +38,13 @@ class Board extends React.Component {
   diselect = () => {
     return setTimeout(() => this.setState( {clicked: 0} ), this.state.timer);
   }
+  toggleMute = () => {
+    console.log('mute :', this.state.mute);
+    return this.setState( {mute: !this.state.mute} );
+  }
 
   render() {
+    const mute = this.state.mute;
     return <>
       <div className="Board">
         {[1,2,3,4].map((id) =>
@@ -44,19 +56,12 @@ class Board extends React.Component {
           />)
         }
       </div>
-      <div>
-        <audio id="simon1">
-          <source src={sound1} type="audio/mpeg" />
-        </audio>
-        <audio id="simon2">
-          <source src={sound2} type="audio/mpeg" />
-        </audio>
-        <audio id="simon3">
-          <source src={sound3} type="audio/mpeg" />
-        </audio>
-        <audio id="simon4">
-          <source src={sound4} type="audio/mpeg" />
-        </audio>
+      <div className="sound">
+        <div className={cn({mute: mute, speaker: !mute,})} onClick={() => this.toggleMute()}> {`Muted: ${mute}`} </div>
+        <audio id="simon1"><source src={sound1} type="audio/mpeg" /></audio>
+        <audio id="simon2"><source src={sound2} type="audio/mpeg" /></audio>
+        <audio id="simon3"><source src={sound3} type="audio/mpeg" /></audio>
+        <audio id="simon4"><source src={sound4} type="audio/mpeg" /></audio>
       </div>
     </>;
   }
