85f466623731f20aba58a847f7045e342b6c8347
Yaniv M <yaniv.michaelis@wework.com>
2019-08-10 23:09:57 +0300

    Updates

---
+++ precache-manifest.287312d146916b617d81932e4bd91e1b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3036698a93d6df1ff7b451057e94a2ae",
+    "url": "/simon/index.html"
+  },
+  {
+    "revision": "eefca9f3e5dc6b81d42e",
+    "url": "/simon/static/css/main.900337a7.chunk.css"
+  },
+  {
+    "revision": "32e73952137d438128b1",
+    "url": "/simon/static/js/2.3d56b785.chunk.js"
+  },
+  {
+    "revision": "eefca9f3e5dc6b81d42e",
+    "url": "/simon/static/js/main.972d5bf6.chunk.js"
+  },
+  {
+    "revision": "fd63f190e946a41f7d5b",
+    "url": "/simon/static/js/runtime~main.24bf3572.js"
+  },
+  {
+    "revision": "45f2ed79035aea5dd4ff92deaacd0698",
+    "url": "/simon/static/media/failure-icon.45f2ed79.svg"
+  },
+  {
+    "revision": "ff4b9f82885ab14427e570b739816696",
+    "url": "/simon/static/media/mute-icon.ff4b9f82.svg"
+  },
+  {
+    "revision": "fca7437c81cd18bfe5f8d20d85303ab5",
+    "url": "/simon/static/media/play-icon.fca7437c.svg"
+  },
+  {
+    "revision": "517c4f7069624dd69307531dd180e3a8",
+    "url": "/simon/static/media/reset-icon.517c4f70.svg"
+  },
+  {
+    "revision": "c40e107e2b240bb9c7be30fe239e532b",
+    "url": "/simon/static/media/simon-icon.c40e107e.svg"
+  },
+  {
+    "revision": "319c77102049da3622a954acd189f1eb",
+    "url": "/simon/static/media/simonSound1.319c7710.mp3"
+  },
+  {
+    "revision": "c3c86e71f309dde162a2e9b52cbc702f",
+    "url": "/simon/static/media/simonSound2.c3c86e71.mp3"
+  },
+  {
+    "revision": "f3c18b98bab33c0fbedde9cb67bdfcf2",
+    "url": "/simon/static/media/simonSound3.f3c18b98.mp3"
+  },
+  {
+    "revision": "f4a912ee52e2c884a80499208c5568cc",
+    "url": "/simon/static/media/simonSound4.f4a912ee.mp3"
+  },
+  {
+    "revision": "502251e64d7d65fdf035640b9ee394b5",
+    "url": "/simon/static/media/speaker-icon.502251e6.svg"
+  },
+  {
+    "revision": "4a27e5f44e1b8c576dbd0fa3186dc9e1",
+    "url": "/simon/static/media/user-icon.4a27e5f4.svg"
+  }
+]);
